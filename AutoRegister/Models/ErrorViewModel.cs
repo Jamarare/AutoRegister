6cc36145e1e30a1cac84854c53c6393eb73dd5c3
Allan Lond <158562268+Jamarare@users.noreply.github.com>
2025-06-13 10:45:27 +0300

    Add project files.

---
+++ AutoRegister/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AutoRegister.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
